aad86e22e3e8ef6e75b9de97c81df70f630a9c89
webeveryday <web3everyday@proton.me>
2024-07-31 15:48:54 -0400

    Example of Test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -5,8 +5,14 @@ pragma solidity ^0.8.18;
 import {Test} from "forge-std/Test.sol";
 
 contract FundMeTest {
-  // This function runs first
-  function setUp() external {}
+  uint256 number = 1;
 
-  function testDemo() public {}
+  // This function runs first
+  function setUp() external {
+    number = 2;
+  }
+
+  function testDemo() public view{
+    assert(number == 2);
+  }
 }
